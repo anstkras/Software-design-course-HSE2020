59c81c7968ef1638eee89dd0307c61051de4fbd7
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2020-02-25 22:30:43 +0300

    add docs

--- CLI/inc/commands/cd_command.h
+++ CLI/inc/commands/cd_command.h
@@ -10,16 +10,32 @@
 
 namespace NCLI::NCommand {
 
+    /**
+     * Command that changes the current path to the given path or
+     * to the HOME environment variable's value if path is not specified
+     */
     class CdCommand : public Command {
     public:
-
+        /**
+         * Command changes the current path to the HOME environment variable's value
+         */
         CdCommand(Environment& env) : env_(env) {};
-
+        /**
+         * Command changes the current path to the given path
+         */
         CdCommand(Environment& env, const std::filesystem::path& path)
             : env_(env), path_(std::filesystem::absolute(path)) {};
-
+        /**
+         * Changes the current path to the given path or
+         * to the HOME environment variable's value if path is not specified
+         * If path is not correct or HOME variable is not set, returns result with ExecutionStatus::error
+         */
         virtual ExecutionResult execute(std::istream& is, std::ostream& os) override;
 
+        /**
+         * If args[1] given, uses it as a path to create the command,
+         * otherwise creates command that uses HOME environment variable
+         */
         static Result<std::shared_ptr<Command>, std::string> create_command(
                 Environment& env, const std::vector<std::string>& args);
     private:
--- CLI/inc/commands/ls_command.h
+++ CLI/inc/commands/ls_command.h
@@ -9,16 +9,36 @@
 
 namespace NCLI::NCommand {
 
+    /**
+     * Command that lists the content of the given path
+     * or of the current directory if path is not specified
+     */
     class LsCommand : public Command {
     public:
-
+        /**
+         * Command lists the content of the current directory to the
+         * output stream
+         */
         LsCommand() : path_(std::filesystem::current_path()) {};
 
+        /**
+         * Command lists the content of the given path to the
+         * output stream
+         */
         LsCommand(const std::filesystem::path& path)
             : path_(std::filesystem::absolute(path)) {};
 
+        /**
+         * Lists the content of the given path or
+         * of the current directory if path is not specified
+         * to the output stream
+         */
         virtual ExecutionResult execute(std::istream& is, std::ostream& os) override;
 
+        /**
+         * If args[1] given, uses it as the path to create command,
+         * otherwise creates ls command that lists the content of the current directory
+         */
         static Result<std::shared_ptr<Command>, std::string> create_command(
                 const std::vector<std::string>& args);
     private:
